2925506bc51229087d1df57dba42bb9025fb5373
Lewis Bell <lbell92@terpmail.umd.edu>
2017-12-12 13:32:55 -0500

    Update coinmap.py
    Expanded help and description. More coloring

--- coinmap.py
+++ coinmap.py
@@ -22,14 +22,14 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 def coinmap():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-n", "--limit", type=int, default=10)
-    parser.add_argument("-s", "--sort_type", default="rank", 
+    parser = argparse.ArgumentParser(description="A program that lists cryptocurrency data sourced from coinmarketcap.com")
+    parser.add_argument("-n", "--limit", type=int, default=10, help="The number of coins to display. If not specified the default is 10")
+    parser.add_argument("-s", "--sort_type", type=str, default="rank", help="Metric to sort by. If not specified the default is rank", 
                         choices=["price_usd", "rank", "market_cap_usd", "vol_usd_24h",
                                  "percentage_change_1h", "percent_change_24h", "percent_change_7d",
                                  "name", "symbol"])
-    parser.add_argument("-r", "--reverse", action="store_true", default=False)
-    parser.add_argument("-c", "--coin", help="Name of specific coin")
+    parser.add_argument("-r", "--reverse", default=False, action="store_true", help="Reverse sort order")
+    parser.add_argument("-c", "--coin", type=str, help="Name of specific coin")
     args = parser.parse_args()
     
     if(args.coin is None):
@@ -55,9 +55,9 @@ def coinmap():
         print("Sorting by: {:}, Reversed: {:}, Limit: {:}".format(args.sort_type, args.reverse, args.limit))
     else:
         print("Selected Coin: {:}".format(args.coin))
-    print("""------------------------------------------------------------------------------------------------------------------------------------------
-║ nR │  SYM  -      Coin      │      Price    │ Change (1H) | Change (24H) │ Change (7D) │    Volume (24H)   │     Market Cap      │ Rank ║
-------------------------------------------------------------------------------------------------------------------------------------------""")        
+    print("""--------------------------------------------------------------------------------------------------------------------------------------------
+║ nR │  SYM  -       Coin       │      Price    │ Change (1H) | Change (24H) │ Change (7D) │    Volume (24H)   │     Market Cap      │ Rank ║
+--------------------------------------------------------------------------------------------------------------------------------------------""")        
     n_rank = 1
     for coin in sorted_list:
         name = coin['name']
@@ -71,22 +71,34 @@ def coinmap():
         market_cap_usd = coin['market_cap_usd']
         
         percent_change_1h_str = "{:^11.2%}".format(percent_change_1h / 100)
-        if (percent_change_1h / 100) < 0 :
+        if percent_change_1h < -5:
             percent_change_1h_str = bcolors.FAIL + percent_change_1h_str + bcolors.ENDC
+        elif percent_change_1h < 0:
+            percent_change_1h_str = bcolors.WARNING + percent_change_1h_str + bcolors.ENDC
+        elif percent_change_1h > 10:
+            percent_change_1h_str = bcolors.OKGREEN + percent_change_1h_str + bcolors.ENDC
             
         percent_change_24h_str = "{:^12.2%}".format(percent_change_24h / 100)
-        if (percent_change_24h / 100) < 0 :
+        if percent_change_24h < -5 :
             percent_change_24h_str = bcolors.FAIL + percent_change_24h_str + bcolors.ENDC
+        elif percent_change_24h < 0 :
+            percent_change_24h_str = bcolors.WARNING + percent_change_24h_str + bcolors.ENDC
+        elif percent_change_24h > 10:
+            percent_change_24h_str = bcolors.OKGREEN + percent_change_24h_str + bcolors.ENDC
             
         percent_change_7d_str = "{:^11.2%}".format(percent_change_7d / 100)
-        if (percent_change_7d / 100) < 0 :
+        if percent_change_7d < -5 :
             percent_change_7d_str = bcolors.FAIL + percent_change_7d_str + bcolors.ENDC
+        elif percent_change_7d < 0 :
+            percent_change_7d_str = bcolors.WARNING + percent_change_7d_str + bcolors.ENDC
+        elif percent_change_7d > 10:
+            percent_change_7d_str = bcolors.OKGREEN + percent_change_7d_str + bcolors.ENDC
         
-        print ("║{:^4}│ {:^5} - {:^14} │ ${:>12,} │ {:} │ {:} │ {:} │ ${:>16,} │ ${:>18,} │ {:^4} ║"
+        print ("║{:^4}│ {:^5} - {:^16} │ ${:>12,} │ {:} │ {:} │ {:} │ ${:>16,} │ ${:>18,} │ {:^4} ║"
                .format(n_rank, symbol, name, price_usd, percent_change_1h_str, 
                        percent_change_24h_str, percent_change_7d_str, vol_usd_24h, market_cap_usd,
                        rank))
-        print("------------------------------------------------------------------------------------------------------------------------------------------")
+        print("--------------------------------------------------------------------------------------------------------------------------------------------")
         n_rank += 1
     print("Data source from coinmarketcap.com at {:}".format(time_stamp))
     
