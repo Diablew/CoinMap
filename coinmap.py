1161afa6fd33dc9dfb476f9c7fb13e2908990cd8
Diablew <lbell73192@gmail.com>
2018-01-18 22:09:17 -0500

    Added sorting by portfolio data

--- coinmap.py
+++ coinmap.py
@@ -75,7 +75,7 @@ def print_headers(args, row_len):
 
     print(bcolors.HEADER + '-'*row_len)
     if args.file:
-        print("║ nR  │  SYM  -       Coin       │     Price    │ Chg (1H) | Chg (1D) │ Chg (7D) │   Volume (1D)   │    Market Cap    │  Profit -  % Chg  │ Amt  Held │  I Inv - Curr Val │ oR  ║")
+        print("║ nR  │  SYM  -       Coin       │     Price    │ Chg (1H) | Chg (1D) │ Chg (7D) │   Volume (1D)   │    Market Cap    │  Profit -  % Chg  │ Amt  Held │   I Inv - Curr Val │ oR  ║")
     else:
         print("║ nR  │  SYM  -       Coin       │     Price    │ Chg (1H) | Chg (1D) │ Chg (7D) │   Volume (1D)   │    Market Cap    │ oR  ║")
     print('-'*row_len + bcolors.ENDC) 
@@ -88,19 +88,43 @@ def print_footers(file, row_len, time_stamp, i_inv, t_profit, t_p_change, n_val,
     else:
         print (row)
     
-def cast_strs(json_resp):
+def cast_n_calc_strs(json_resp, portfolio):
     for coin in json_resp:
-            coin['rank'] = int(coin['rank'])
-            coin['price_usd'] = float(coin['price_usd'])
-            coin['24h_volume_usd'] = float(coin['24h_volume_usd'])
-            coin['percent_change_1h'] = float(coin['percent_change_1h'])
-            coin['percent_change_24h'] = float(coin['percent_change_24h'])
-            coin['percent_change_7d'] = float(coin['percent_change_7d']) # FIX: breaks when n > 200, sometimes less
-            coin['market_cap_usd'] = 0.0 if (coin['market_cap_usd'] is None) else float(coin['market_cap_usd'])
+        coin['rank'] = int(coin['rank'])
+        coin['price_usd'] = float(coin['price_usd'])
+        coin['24h_volume_usd'] = float(coin['24h_volume_usd'])
+        coin['percent_change_1h'] = float(coin['percent_change_1h'])
+        coin['percent_change_24h'] = float(coin['percent_change_24h'])
+        coin['percent_change_7d'] = float(coin['percent_change_7d']) # FIX: breaks when n > 200, sometimes less
+        coin['market_cap_usd'] = 0.0 if (coin['market_cap_usd'] is None) else float(coin['market_cap_usd'])
+        if portfolio is not None:
+            for c in portfolio:
+                p_change = 0.0
+                profit = 0.0
+                amt_held = 0.0
+                curr_val = 0.0
+                inv = 0.0
+                if(coin['name'] in portfolio):
+                    #p_size += 1
+                    o_val = 0.0
+                    c_val = 0.0
+                    for t in portfolio[coin['name']]:
+                        amt_held += t[1]
+                        o_val += t[1] * t[2]            # amt * price paid
+                        c_val += t[1] * float(coin['price_usd'])       # amt * current price
+                        #print("{}\n\t\to_val:{}\n\t\tc_val:{}".format(t, o_val, c_val))
+                    p_change, profit = get_p_change(o_val, c_val)
+                    inv += o_val
+                    curr_val += c_val
+                coin['p_change'] = p_change
+                coin['profit'] = profit
+                coin['amt_held'] = amt_held
+                coin['curr_val'] = curr_val
+                coin['inv'] = inv
 #================================================================================================================
 #TODO:
-#Store balances & transactions, calculate total earn/loss
-#Read csv
+#       Make row_len dynamic
+#       Bold/highlight every other line    
 #================================================================================================================
 def coinmap():
     # initialize argument parser
@@ -109,7 +133,7 @@ def coinmap():
     parser.add_argument("-s", "--sort_type", type=str, default="rank", help="Metric to sort by. If not specified the default is rank", 
                         choices=["price_usd", "rank", "market_cap_usd", "24h_volume_usd",
                                  "percent_change_1h", "percent_change_24h", "percent_change_7d",
-                                 "name", "symbol"])
+                                 "name", "symbol", "inv", "profit", "p_change", "curr_val"])
     parser.add_argument("-r", "--reverse", default=False, action="store_true", help="Reverse sort order")
     parser.add_argument("-c", "--coin", type=str, nargs="+", help="Name of specific coin")
     parser.add_argument("-f", "--file", help="csv transaction history")
@@ -118,10 +142,11 @@ def coinmap():
     json_resp = []
     url = ""
     time_stamp = datetime.now()
+    portfolio = None
     # gather data from csv if file argument is passed
     if (args.file):
         portfolio = read_csv(args.file)
-        row_len = 177
+        row_len = 178
     else:
         row_len = 125
     n_val = 0
@@ -149,14 +174,14 @@ def coinmap():
             if 'error' in json_resp:    
                 print ("url: {}\njson_resp: {}".format(get(u), json_resp))
                 return
-    # cast string values from JSON to correct type for sorting
-    cast_strs(json_resp)
-    
+    # cast string values from JSON to correct type for sorting 
+    cast_n_calc_strs(json_resp, portfolio)
+
     # sort compiled list by selected metric
     if args.sort_type != 'rank' and args.sort_type != 'name':
         args.reverse = not args.reverse
     sorted_list = sorted(json_resp, key=lambda x: x[args.sort_type], reverse=args.reverse)
-
+    
     print_headers(args, row_len)
     
     for n_rank, coin in enumerate(sorted_list, start=1):
@@ -170,31 +195,7 @@ def coinmap():
         percent_change_24h = float(coin['percent_change_24h'])
         percent_change_7d = float(coin['percent_change_7d'])
         market_cap_usd = float(coin['market_cap_usd']) 
-        
-        # Calculate net value and percent change
-        p_change = None
-        profit = None
-        amt_held = 0.0
-        curr_val = 0.0
-        inv = 0.0
-        if (args.file):
-            if(name in portfolio):
-                p_size += 1
-                o_val = 0.0
-                c_val = 0.0
-                for t in portfolio[name]:
-                    amt_held += t[1]
-                    o_val += t[1] * t[2]            # amt * price paid
-                    c_val += t[1] * price_usd       # amt * current price
-                    #print("{}\n\t\to_val:{}\n\t\tc_val:{}".format(t, o_val, c_val))
-                p_change, profit = get_p_change(o_val, c_val)
-                t_profit += profit
-                avg_p_change += p_change
-                inv += o_val
-                i_inv += o_val
-                curr_val += c_val
-                #print("profit:{}\nt_profit:{}\np_change:{}\navg_p_change: {}".format(profit, t_profit, p_change, avg_p_change))
-        
+
         # sorting by rank or name, forward - or - num, reverse
         if ((args.sort_type == 'rank' or args.sort_type == 'name') and not args.reverse) or \
             (args.sort_type != 'rank' and args.sort_type != 'name' and args.reverse):
@@ -216,7 +217,7 @@ def coinmap():
         #vol_str = colorize(vol_usd_24h, 'bold', "${:>16,}".format(vol_usd_24h))
         #mcap_str = colorize(market_cap_usd, 'bold', "${:>18,}".format(market_cap_usd))
         
-        ## TODO: Bold/highlight every other line
+        ## Bold/highlight every other line attempt #1
         #if (n_rank % 2 == 0):
         #    for i, s in enumerate(strs):
         #        if u
@@ -224,9 +225,20 @@ def coinmap():
         # store values
         coin_vals[name] = [n_rank, symbol, price_usd, percent_change_1h, percent_change_24h, percent_change_7d,                   vol_usd_24h, market_cap_usd, rank]
         if (args.file):
+            amt_held = coin['amt_held']
+            inv = coin['inv']
+            curr_val = coin['curr_val']
+            profit = coin['profit']
+            p_change = coin['p_change']
+            if(name in portfolio):
+                i_inv += coin['inv']
+                p_size += 1
+                t_profit += profit
+                avg_p_change += p_change
+        
             amt_held_str = "{:>9.4f}".format(amt_held) # if amt_held else "   -N/A-   "
             inv_str = "{:>7.2f}".format(inv)
-            curr_val_str = "{:>7.2f}".format(curr_val)
+            curr_val_str = "{:>8.2f}".format(curr_val) if curr_val else "  -N/A- "
            
             profit_str = colorize(profit, None, "{:<7.2f}".format(profit)) if profit else " -N/A- "
             p_change_str = colorize(p_change * 100, None, "{:>7.2%}".format(p_change)) if p_change else " -N/A- "
@@ -239,7 +251,6 @@ def coinmap():
         str_len = 0
         for s in strs:
             str_len += len(s)
-        ## TODO: make row_len dynamic
         ## str_len - 1 == row_len
     if(args.file):
         avg_p_change /= p_size if p_size > 0 else 1
